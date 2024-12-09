7d65e0594795973edd9fd2634bd7ca6bfe1086e0
ELCANET\hoal <lam.hoang@elca.vn>
2024-12-09 22:55:32 +0700

    test

--- script/script.sh
+++ script/script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e  # Stop the script if any command fails
+
+echo "Environment variables:"
 printenv
-echo Testing entrypoint
-aws s3 cp s3://testbuckethoanglam147/p17/vault-P17 - Top 6 tests-1733466316298.json /app/config.json
+
+echo "Copying config file from S3..."
+aws s3 cp s3://testbuckethoanglam147/p17/vault-P17 - Top 6 tests-1733466316298.json /app/config.json || {
+    echo "Failed to copy config from S3";
+    exit 1;
+}
+
+echo "Running Maven tests..."
 mvn test
