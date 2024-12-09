38780544927dc93ac5409bdbbcfbd5385b1dc167
ELCANET\hoal <lam.hoang@elca.vn>
2024-12-09 23:01:27 +0700

    test

--- script/script.sh
+++ script/script.sh
@@ -5,7 +5,7 @@ echo "Environment variables:"
 printenv
 
 echo "Copying config file from S3..."
-aws s3 cp s3://testbuckethoanglam147/p17/vault-P17 - Top 6 tests-1733466316298.json /app/config.json || {
+aws s3 cp s3://testbuckethoanglam147/p17/a1.json /app/config.json || {
     echo "Failed to copy config from S3";
     exit 1;
 }
