26412272b2e241fbf6f4d6857d322319fb652340
ELCANET\hoal <lam.hoang@elca.vn>
2024-12-11 22:35:48 +0700

    test

---
+++ src/test/java/com/secutix/museum/tickets/Test3.java
@@ -0,0 +1,12 @@
+package com.secutix.museum.tickets;
+import com.secutix.museum.AbstractConfigTest;
+import org.junit.jupiter.api.Test;
+
+//@ExtendWith(RetryExtension.class)
+public class Test3 extends AbstractConfigTest {
+    @Test
+    public void testPostEndpoint() {
+        System.out.println("This is a test3");
+    }
+
+}
