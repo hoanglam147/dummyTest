dcddb27d2c24de6c64df175250727d97bfd67371
ELCANET\hoal <lam.hoang@elca.vn>
2024-12-11 09:23:29 +0700

    test

--- script/script.sh
+++ script/script.sh
@@ -6,7 +6,8 @@ printenv
 
 # Use the CONFIG_FILE_PATH environment variable
 echo "Using configuration file: $CONFIG_FILE"
-
+S3_BUCKET_HISTORY="s3 cp s3://testbuckethoanglam147/$ENV/allure-report/history"
+S3_BUCKET="s3 cp s3://testbuckethoanglam147/$ENV/allure-report/history"
 # Perform operations with CONFIG_FILE_PATH
 
 echo "Copying config file from S3..."
@@ -17,3 +18,24 @@ aws s3 cp $CONFIG_FILE /app/config.json || {
 
 echo "Running Maven tests..."
 mvn test
+
+# add allure path
+curl -o allure-latest.zip https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.30.0/allure-commandline-2.30.0.zip
+unzip allure-latest.zip -d /opt/
+export PATH=$PATH:/opt/allure-2.30.0/bin
+
+echo 'export PATH=$PATH:/opt/allure-2.30.0/bin' >> ~/.bashrc
+source ~/.bashrc
+# check version
+allure --version
+cd target/
+if aws s3 ls "${S3_BUCKET_HISTORY}" > /dev/null 2>&1; then
+    echo "Directory exists in S3: ${S3_BUCKET}"
+    aws s3 cp ${S3_BUCKET_HISTORY} ./allure-results/history/ --recursive
+else
+    echo "Directory does not exist in S3: ${S3_BUCKET_HISTORY}"
+fi
+
+allure generate
+aws s3 cp ./allure-report ${S3_BUCKET_HISTORY} --recursive
+git clean -f -d
