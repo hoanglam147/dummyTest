5eb7e5e183f15649dfdba3ee53b109b48c8ff22a
ELCANET\hoal <lam.hoang@elca.vn>
2024-12-11 23:50:41 +0700

    test

--- script/script.sh
+++ script/script.sh
@@ -8,7 +8,7 @@ printenv
 echo "Using configuration file: $CONFIG_FILE"
 S3_BUCKET_HISTORY="s3://testbuckethoanglam147/$ENV/allure-report/history"
 S3_BUCKET="s3://testbuckethoanglam147/$ENV/allure-report"
-S3_BUCKET_RESULT="s3://testbuckethoanglam147/$ENV/allure-result"
+S3_BUCKET_RESULT="s3://testbuckethoanglam147/$ENV/allure-result/"
 # Perform operations with CONFIG_FILE_PATH
 
 echo "Copying config file from S3..."
@@ -19,6 +19,9 @@ aws s3 cp $CONFIG_FILE /app/config.json || {
 
 echo "Running Maven tests..."
 mvn clean test
+current_datetime=$(date +"%Y-%m-%d-%H-%M-%S")
+echo current_datetime
+aws s3 cp ./allure-results ${S3_BUCKET_RESULT}/${current_datetime} --recursive
 
 # add allure path
 curl -o allure-latest.zip https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.30.0/allure-commandline-2.30.0.zip
@@ -39,5 +42,3 @@ fi
 allure --version
 allure generate || { echo "Allure generation failed"; exit 1; }
 aws s3 cp ./allure-report ${S3_BUCKET} --recursive
-current_datetime=$(date +"%Y-%m-%d-%H-%M-%S")
-aws s3 cp ./allure-results ${S3_BUCKET_RESULT}/${current_datetime} --recursive
