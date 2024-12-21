70844d9592d27cec10ddc8fc076a3922a9ee8b40
ELCANET\hoal <lam.hoang@elca.vn>
2024-12-22 00:18:51 +0700

    test

--- src/test/java/com/secutix/museum/tickets/Import_Contr_TicketTest.java
+++ src/test/java/com/secutix/museum/tickets/Import_Contr_TicketTest.java
@@ -1,11 +1,12 @@
 package com.secutix.museum.tickets;
 import com.secutix.museum.AbstractConfigTest;
+import com.secutix.rule.RetryExtension;
 import org.junit.jupiter.api.Test;
 
 import com.secutix.util.Configuration;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-//@ExtendWith(RetryExtension.class)
+@ExtendWith(RetryExtension.class)
 public class Import_Contr_TicketTest extends AbstractConfigTest {
     @Test
     public void testPostEndpoint() {
